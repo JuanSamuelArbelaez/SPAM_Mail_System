837615511ab4c9f23c2c03a5557471731687cfe7
JuanSamuelArbelaez <109239223+JuanSamuelArbelaez@users.noreply.github.com>
2024-04-17 21:30:16 -0500

    Initial SpringBoot Application Commit

---
+++ src/main/java/juan/arbelaez/spam_mail_system/SpamMailSystemApplication.java
@@ -0,0 +1,13 @@
+package juan.arbelaez.spam_mail_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpamMailSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpamMailSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/juan/arbelaez/spam_mail_system/SpamMailSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package juan.arbelaez.spam_mail_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpamMailSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
