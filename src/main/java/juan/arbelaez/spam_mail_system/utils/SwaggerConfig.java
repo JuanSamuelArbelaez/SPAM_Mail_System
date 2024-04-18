fc809ac66c9084fe59e186f1f3eecc293102ab97
JuanSamuelArbelaez <109239223+JuanSamuelArbelaez@users.noreply.github.com>
2024-04-18 08:49:03 -0500

    Swagger config file added

---
+++ src/main/java/juan/arbelaez/spam_mail_system/utils/SwaggerConfig.java
@@ -0,0 +1,16 @@
+package juan.arbelaez.spam_mail_system.utils;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SecurityScheme(
+        name = "bearerAuth",
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        type = SecuritySchemeType.HTTP,
+        in = SecuritySchemeIn.HEADER
+)
+public class SwaggerConfig {
+}
